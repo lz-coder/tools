b5afa157f33266136a445675eb96c595134c0c27
lz-coder <lzcoder@proton.me>
2024-07-30 01:37:07 -0300

    setup for meslo nerd font

---
+++ lzmgr/setup/nerd_fonts_meslo.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ ! -f /bin/unzip ]; then
+	echo "unzip is needed, install it!!!";
+	exit 1
+fi
+
+getcmd="curl -Lo"
+
+if [ ! -d $HOME/.local/share/fonts ]; then
+	mkdir -p $HOME/.local/share/fonts
+fi
+
+action=""
+message=""
+if [ -d $HOME/.local/share/fonts/meslo-nerd ]; then
+	message="updated"
+	action="updating"
+else
+	message="installed"
+	action="updating"
+fi
+
+echo
+echo "$action Meslo Nerd Font"
+$getcmd $HOME/.local/share/fonts/Meslo.zip "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip"
+unzip $HOME/.local/share/fonts/Meslo.zip -d $HOME/.local/share/fonts/meslo-nerd
+rm -rf $HOME/.local/share/fonts/Meslo.zip
+fc-cache -f
+
+echo "Meslo Nerd Font $message"
+
