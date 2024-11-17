89438fa3428e76a19a1070acffb1dddeed030dbf
lz-coder <lzcoder@proton.me>
2024-11-17 03:35:33 -0300

    add new script to configure git

---
+++ scripts/simple/git-configs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Configuring git"
+git config --global user.name lz-coder
+echo "username configured"
+git config --global user.email lzcoder@proton.me
+echo "email configured"
+git config --global init.defaultBranch main
+echo "default branch name configured"
